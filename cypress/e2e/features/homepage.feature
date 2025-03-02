9677cb29107cfd8f4a14adf13b30adb7762b5444
Marrbe <132088541+marbhg@users.noreply.github.com>
2025-03-02 13:02:34 +0100

    ACT
    ACT

--- cypress/e2e/features/homepage.feature
+++ cypress/e2e/features/homepage.feature
@@ -1,7 +1,15 @@
-Feature: Open Bootcamp QA
+Feature: Open Buggy Cars
 
     As studen, I want to open bootcamp qa, so I can see training courses
 
 
-    Scenario: Visit Bootcamp QA Web
-        When the user visits homepage
+    Scenario: Visit Buggy Cars positive 
+        Given the user visits homepage
+        When As a user I enter the registration section 
+        And the user fills in the login "mar@gmail.com"
+        And the user fills in the first name "Maria"
+        And the user fills in the last name "Lopez"
+        And the user fills in the password "Password123!"
+        And the user fills in the first confirm password "Password123!"
+        Then I give the register button 
+       
--- cypress/e2e/step_definitions/homepage_steps.js
+++ cypress/e2e/step_definitions/homepage_steps.js
@@ -4,9 +4,51 @@ import {
     Then,
   } from "@badeball/cypress-cucumber-preprocessor";
   
-
+//Primero visita la pagina web 
   When("the user visits homepage", () => {
     cy.visit("/");
   });
+//Entra en la session de registro y hace click
+  When("As a user I enter the registration section",  () => {
+    cy.contains("Register").click()
+  });
+
+  //Rellena los campos del formulario 
+  When("the user fills in the login {string}", (login) => {
+    console.log("Valor de login:", login);
+    cy.get('[formControlName="username"]').type(login);
+  });
+
+  When("the user fills in the first name {string}", (firstname) => {
+    console.log("Valor de first name:", firstname);
+    cy.get('[name="firstName"]').type(firstname);
+  });
+
+  When("the user fills in the last name {string}", (lastname) => {
+    console.log("Valor de first name:", lastname);
+    cy.get('[name="lastName"]').type(lastname);
+  });
+
+  When("  the user fills in the password  {string}", (password) => {
+    console.log("Valor de confirm password:", password);
+    cy.get('[name="password"]').type(password); 
+});
+
+When("the user fills in the confirm password {string}", (confirmpassword) => {
+    console.log("Valor de confirm password:", confirmpassword);
+    cy.get('[name="confirmPassword"]').type(confirmpassword); 
+});
+
+When("I give the register button", () => {
+    cy.contains("Register").click(); 
+});
+
+
+
+
+
+
+
+
 
  
